c10366f35c6100d7674107945d543ec451cbe3f7
Yosip Mike Colin <colinparrado@gmail.com>
2024-06-19 16:05:55 -0500

    Adding database in the cloud

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,8 +1,19 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("brm", "root", "", {
+// * PRODUCTION
+const db = new Sequelize(
+  "mysql://root:UhOqIWsXuTYwUxayOmTRozBycxGxmSEZ@roundhouse.proxy.rlwy.net:46434/railway",
+  {
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+// * DEVELOPMENT
+/* const db = new Sequelize("brm", "root", "", {
   host: "localhost",
   dialect: "mysql",
-});
+  logging: false
+}); */
 
 export default db;
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,34 +1,40 @@
 import { DataTypes } from "sequelize";
 import db from "../database/connection.js";
 
-const User = db.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username: { type: DataTypes.STRING },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: { type: DataTypes.STRING, allowNull: false },
-  role: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      isIn: [["admin", "client"]],
+const User = db.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: { type: DataTypes.STRING },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["admin", "client"]],
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
     },
   },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "users",
+  }
+);
 
 export default User;
